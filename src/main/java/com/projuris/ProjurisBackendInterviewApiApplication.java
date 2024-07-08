933c77222da43c4fa411b853cfe220a8ab48deb2
lazaro.silva <lazaro.silva@datainfo.inf.br>
2024-07-08 20:30:55 +0100

    removendo o número 1 do application

--- src/main/java/com/projuris/ProjurisBackendInterviewApiApplication.java
+++ src/main/java/com/projuris/ProjurisBackendInterviewApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjurisBackendInterviewApi1Application {
+public class ProjurisBackendInterviewApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjurisBackendInterviewApi1Application.class, args);
+		SpringApplication.run(ProjurisBackendInterviewApiApplication.class, args);
 	}
 
 }
